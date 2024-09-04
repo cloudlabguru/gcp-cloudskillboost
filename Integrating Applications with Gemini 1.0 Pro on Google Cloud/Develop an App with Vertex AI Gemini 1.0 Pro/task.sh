54e4f902cb1c46f6909b2169a69252cc492bf317
cloudlabguru <cloudlabguru@gmail.com>
2024-09-04 23:09:48 +0700

    Update task.sh

--- Integrating Applications with Gemini 1.0 Pro on Google Cloud/Develop an App with Vertex AI Gemini 1.0 Pro/task.sh
+++ Integrating Applications with Gemini 1.0 Pro on Google Cloud/Develop an App with Vertex AI Gemini 1.0 Pro/task.sh
@@ -537,13 +537,6 @@ def render_video_playground_tab(multimodal_model_pro: GenerativeModel):
             st.write(prompt,"\n","{video_data}")
 EOF
 
-
-SERVICE_NAME='gemini-app-playground' # Name of your Cloud Run service.
-AR_REPO='gemini-app-repo'            # Name of your repository in Artifact Registry that stores your application container image.
-
-gcloud artifacts repositories create "$AR_REPO" --location="$REGION" --repository-format=Docker
-gcloud auth configure-docker "$REGION-docker.pkg.dev"
-
 cat > ~/gemini-app/Dockerfile <<EOF
 FROM python:3.8
 
@@ -557,18 +550,6 @@ RUN pip install -r requirements.txt
 ENTRYPOINT ["streamlit", "run", "app.py", "--server.port=8080", "--server.address=0.0.0.0"]
 
 EOF
-
-gcloud builds submit --tag "$REGION-docker.pkg.dev/$PROJECT_ID/$AR_REPO/$SERVICE_NAME"
-
-gcloud run deploy "$SERVICE_NAME" \
-  --port=8080 \
-  --image="$REGION-docker.pkg.dev/$PROJECT_ID/$AR_REPO/$SERVICE_NAME" \
-  --allow-unauthenticated \
-  --region=$REGION \
-  --platform=managed  \
-  --project=$PROJECT_ID \
-  --set-env-vars=PROJECT_ID=$PROJECT_ID,REGION=$REGION
-
   
 streamlit run app.py \
 --browser.serverAddress=localhost \
