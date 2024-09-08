a12d823e8cf90f9640aaed3acda689c5541b47ff
cloudlabguru <cloudlabguru@gmail.com>
2024-09-08 19:27:44 +0700

    Create GSP972.sh

---
+++ Identify Damaged Car Parts with Vertex AutoML Vision/GSP972.sh
@@ -0,0 +1,21 @@
+export PROJECT_ID=$DEVSHELL_PROJECT_ID
+export BUCKET=$PROJECT_ID
+
+gsutil mb -p $PROJECT_ID \
+    -c standard    \
+    -l "REGION" \
+    gs://${BUCKET}
+
+gsutil -m cp -r gs://car_damage_lab_images/* gs://${BUCKET}
+
+gsutil cp gs://car_damage_lab_metadata/data.csv .
+
+sed -i -e "s/car_damage_lab_images/${BUCKET}/g" ./data.csv
+
+gsutil cp ./data.csv gs://${BUCKET}
+
+sleep 20
+
+wget https://raw.githubusercontent.com/Techcps/GSP-Short-Trick/main/Identify%20Damaged%20Car%20Parts%20with%20Vertex%20AutoML%20Vision/payload.json
+
+cat > 
